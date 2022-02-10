b61c09b1ad53f0eefb87d6d84c3a43c9e17c1411
LUIS\Luis <luispicazomartinez@gmail.com>
2022-02-10 19:10:54 +0100

    Primera versión de la API de Twitter

--- social_networks_term_inspector/social_apis/engagement/EngagementExampleAPI.py
+++ social_networks_term_inspector/social_apis/engagement/EngagementExampleAPI.py
@@ -1,6 +1,6 @@
 from response.EngagementResponse import *
 
-class APITwitter:
+class APIExample:
     def getEngagement(user):
         ## Hacemos la llamada a la API correspondiente, realizamos los calculos 
         # necesarios y devolvemos el objeto EngagementResponse con los valores
@@ -8,5 +8,5 @@ class APITwitter:
 
 
 #print(APITwitter.getEngagement('Auron'))
-response = APITwitter.getEngagement('Auron')
+response = APIExample.getEngagement('Auron')
 print(response.data['engagement'])
